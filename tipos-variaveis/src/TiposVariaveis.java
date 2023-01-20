464c35ea5aa713d92f8f5ee9e4c8a8978dcaee36
Efremfz <efrem_me@hotmail.com>
2023-01-20 12:37:16 -0300

    Teste de comportamento de algumas variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       double salarioMinimo = 2500.33;
+       
+       
+       //neste exemplo, temntei atribuir um lavor a de tipos diferentes, foi o usaso o conceito de cast para alterar o tipo da variável 
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 =  (short) numeroNormal;
+
+       //exemplos simples de alteração de valor de uma variável
+       int numero = 5;
+       numero = 10;
+       System.out.println(numero);
+
+       //Exemplode váriável com valor imutavel(constante), neste caso é usado o nome de variável maiucula e com  a palavra reservada (FINAL)
+
+       final double VALOR_DE_PI = 3.14;
+       //Ao usar o final em uma váriável, não será permitido atribuir um  novo valor, vai dar erro 
+       VALOR_DE_PI = 10.14;
+
+    
+
+
     }
 }
