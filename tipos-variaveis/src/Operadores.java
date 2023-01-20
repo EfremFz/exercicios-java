8aba966fb5cf3c1448f4776a354abf74adecb26f
Efremfz <efrem_me@hotmail.com>
2023-01-20 13:17:18 -0300

    Criada a classe Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+
+//O sinal de igual(=) é usando como operador de atribuição
+
+//Para atribuir uma string é necessário usar as aspa dupla "TEXTO"
+String nome = "GLEYSON";
+//Valores como int e double são declarados diretamente, mas o double não interpreta a virgula, ele usa o ponto 
+int idade = 22;
+double peso = 68.5;
+// o char aceita somente um caracter e usa aspa simples na atribuição 'TEXTO'
+char sexo = 'M';
+//Boleano trabalha o os valores verdadeiro(True) e falso(False), por padrão, caso não atribua um valor ele vem como false. 
+boolean doadorOrgao = false;
+//Declaração de classe
+Date dataNascimento = new Date();
+
+
+//Exemplos de opoerações matematicas usandos operadores aritimeticos (+)soma , (-)subitração, (*)multiplicação, (/)Divisão e (%)mod "esse operador verifica o resto da divisão"
+double soma = 10.5 + 15.7;
+int subtração = 113 - 25;
+int multiplicacao = 20 * 7;
+int divisao = 15 / 3;
+int modulo = 18 % 3;
+double resultado = (10 * 7) + (20/4);
+    
+}
