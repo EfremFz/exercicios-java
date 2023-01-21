f396b275610d6a898f875ee2e68f90c804970eef
Efremfz <efrem_me@hotmail.com>
2023-01-21 18:25:16 -0300

    Add exemplos de operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -81,7 +81,33 @@ double resultado = (10 * 7) + (20/4);
 
             System.out.println(resposta);
 
+        //Operadores relacionais
 
+        /*  == Quando desejamos verificar se uma variável é IGUAL A outra.
+            != Quando desejamos verificar se uma variável é DIFERENTE da outra.
+            > Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+            >= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+            < Quando desejamos verificar se uma variável é MENOR QUE outra.
+            <= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra 
+        */
+
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        if(numero1 > numero2)
+            System.out.println("Numero 1 maior que numero 2");
+        
+        if(numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+        
+        if(numero1 >= numero2)
+            System.out.println("Numero 1 maior ou igual que numero 2");
+        
+        if(numero1 <= numero2)
+            System.out.println("Numero 1 menor ou igual que numero 2");
+        
+        if(numero1 != numero2)
+            System.out.println("Numero 1 é diferente de numero 2");
 
     }
     
