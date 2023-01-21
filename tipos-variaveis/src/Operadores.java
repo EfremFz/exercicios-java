9b96c2dfd81c7a1006b1482fb65d4c132f60c7bb
Efremfz <efrem_me@hotmail.com>
2023-01-21 18:35:48 -0300

    Add ex mais complexos de operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -91,23 +91,53 @@ double resultado = (10 * 7) + (20/4);
             <= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra 
         */
 
-        int numero1 = 1;
-        int numero2 = 2;
+            int numero1 = 1;
+            int numero2 = 2;
+            
+            if(numero1 > numero2)
+                System.out.println("Numero 1 maior que numero 2");
+            
+            if(numero1 < numero2)
+                System.out.println("Numero 1 menor que numero 2");
+            
+            if(numero1 >= numero2)
+                System.out.println("Numero 1 maior ou igual que numero 2");
+            
+            if(numero1 <= numero2)
+                System.out.println("Numero 1 menor ou igual que numero 2");
+            
+            if(numero1 != numero2)
+                System.out.println("Numero 1 é diferente de numero 2");
+
+        //Comparações mais avançadas
         
-        if(numero1 > numero2)
-            System.out.println("Numero 1 maior que numero 2");
+       
+            String nome1 = "JAVA";
+            String nome2 = "JAVA";
+            
+            System.out.println(nome1 == nome2); 
+    
+            String nome3 = new String("JAVA");
+            
+            System.out.println(nome1 == nome3); 
+    
+            String nome4 = nome3;
+    
+            System.out.println(nome3 == nome4); 
+            
+            //equals serve para comparar valores dentro de objetos
+            System.out.println(nome1.equals(nome2));
+            System.out.println(nome2.equals(nome3));
+            System.out.println(nome3.equals(nome4));
+
+
+
+        //Operadores Lógicos 
         
-        if(numero1 < numero2)
-            System.out.println("Numero 1 menor que numero 2");
-        
-        if(numero1 >= numero2)
-            System.out.println("Numero 1 maior ou igual que numero 2");
-        
-        if(numero1 <= numero2)
-            System.out.println("Numero 1 menor ou igual que numero 2");
-        
-        if(numero1 != numero2)
-            System.out.println("Numero 1 é diferente de numero 2");
+        /*
+
+         */
+
 
     }
     
