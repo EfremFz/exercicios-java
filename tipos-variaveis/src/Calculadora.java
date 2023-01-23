d66e0ced845292a07fef306c307fda1482930ce3
Efremfz <efrem_me@hotmail.com>
2023-01-22 21:20:18 -0300

    Criada calculadora com comentários de documentação

---
+++ tipos-variaveis/src/Calculadora.java
@@ -0,0 +1,24 @@
+//Os momentarios desta classe usam a seguinte estrurura:( /** COMENTARIOS */ ), esse tipo de comentário é usado quando vamos criar documentação
+
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Efrem Ferreira
+* @version 1.0
+* @since   22/01/2023
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
